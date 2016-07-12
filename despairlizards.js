6c45197e87511f43e7e22c3b5ba63ac55f7f2d75
Kathleen Higgins <Kathleen Higgins>
2016-07-11 21:38:52 -0400

    Add double bounce cup feature

--- despairlizards.js
+++ despairlizards.js
@@ -8,7 +8,9 @@ function beerPong() {
 	} else if (toss > 5 && toss <= 8) {
 		result = "missed!";
 	} else {
-		result = "double bounced!";
+		var cup1 = whichCup();
+		var cup2 = whichCup();
+		result = "double bounced and made it in cup # " + cup1 + " and I pulled cup # " + cup2;
 	} 
 	var words = "I threw for cup # " + cup + " and I " + result;
 	displayResults(words);
@@ -17,4 +19,12 @@ beerPong();
 
 function displayResults(words) {
 	document.getElementById("results").innerHTML = words;
+}
+function whichCup() {
+	var cup1 = Math.random() * 10;
+	cup1 = Math.floor(cup1);
+	cup1 = cup1 + 1;
+	cup1 = cup1 / 2;
+	cup1 = Math.floor(cup1) + 1;
+	return cup1;
 }
