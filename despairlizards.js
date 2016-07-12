c8d32223f30dd19071e30d2062e85294e5230254
Kathleen Higgins <Kathleen Higgins>
2016-07-11 21:22:37 -0400

    add cup select option display throw details in results

--- despairlizards.js
+++ despairlizards.js
@@ -1,17 +1,16 @@
 function beerPong() {
+	var cup = document.getElementById("cup").value;
 	var toss = Math.random() * 10;
 	toss = Math.floor(toss);
 	var result = "";
 	if (toss <= 5) {
-		result = "made it!";
+		result = "made it in cup # " + (toss + 1);
 	} else if (toss > 5 && toss <= 8) {
 		result = "missed!";
 	} else {
 		result = "double bounced!";
 	} 
-	console.log("I " + result);
-	console.log("I rolled a " + toss);
-	var words = "I threw for a cup and I " + result;
+	var words = "I threw for cup # " + cup + " and I " + result;
 	displayResults(words);
 }
 beerPong();
