060902b6f4b4ec80383900583c267f56688368a9
Kathleen Higgins <Kathleen Higgins>
2016-07-11 20:31:41 -0400

    Add beerpong

---
+++ despairlizards.js
@@ -0,0 +1,21 @@
+function beerPong() {
+	var toss = Math.random() * 10;
+	toss = Math.floor(toss);
+	var result = "";
+	if (toss <= 5) {
+		result = "made it!";
+	} else if (toss > 5 && toss <= 8) {
+		result = "missed!";
+	} else {
+		result = "double bounced!";
+	} 
+	console.log("I " + result);
+	console.log("I rolled a " + toss);
+	var words = "I threw for a cup and I " + result;
+	displayResults(words);
+}
+beerPong();
+
+function displayResults(words) {
+	document.getElementById("results").innerHTML = words;
+}
